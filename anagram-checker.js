3523faf00803c26cc0ebbc450aaf4f09914ae6fe
Ibranista <ibranista88@gmail.com>
2022-11-10 15:35:35 +0100

    added a return statement.

--- anagram-checker.js
+++ anagram-checker.js
@@ -2,7 +2,5 @@ module.exports = function (x,y) {
   // write your code here
   let first = x.replace(/[^a-z0-9]/gi, "");
   let second = y.replace(/[^a-z0-9]/gi, "");
-  first.split("").sort().join("") === second.split("").sort().join("")
-    ? true
-    : flase;
+  return first.split("").sort().join("") === second.split("").sort().join("")
 };
