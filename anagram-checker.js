66b8ff255dc5c2f5b9b7c810178d52a7ee14597b
Ibranista <ibranista88@gmail.com>
2022-11-10 15:29:25 +0100

    added turnary operator.

--- anagram-checker.js
+++ anagram-checker.js
@@ -2,5 +2,7 @@ module.exports = function (str) {
   // write your code here
   let first = x.replace(/[^a-z0-9]/gi, "");
   let second = y.replace(/[^a-z0-9]/gi, "");
-  return first.split("").sort().join("") === second.split("").sort().join("");
+  first.split("").sort().join("") === second.split("").sort().join("")
+    ? true
+    : flase;
 };
