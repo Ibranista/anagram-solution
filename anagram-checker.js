9ca9ef02ff00c4c3045e6798f26052078c1de374
Ibranista <ibranista88@gmail.com>
2022-11-10 14:48:52 +0100

    created solution for anagram problem.

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,3 +1,6 @@
 module.exports = function (str) {
   // write your code here
-}
+  let first = x.replace(/[^a-z0-9]/gi, "");
+  let second = y.replace(/[^a-z0-9]/gi, "");
+  return first.split("").sort().join("") === second.split("").sort().join("");
+};
