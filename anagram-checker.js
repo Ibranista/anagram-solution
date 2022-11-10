d96da51448e60fb0cc9fe1e49797f91c60689670
Ibranista <ibranista88@gmail.com>
2022-11-10 16:44:17 +0100

    added toLowerCase method on the received strings.

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,6 +1,9 @@
-module.exports = function (x,y) {
+module.exports = function (x, y) {
   // write your code here
   let first = x.replace(/[^a-z0-9]/gi, "");
   let second = y.replace(/[^a-z0-9]/gi, "");
-  return first.split("").sort().join("") === second.split("").sort().join("")
+  return (
+    first.toLowerCase().split("").sort().join("") ===
+    second.toLowerCase().split("").sort().join("")
+  );
 };
